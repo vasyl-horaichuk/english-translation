03e23bb9a43e7526802796610f5fdad73fa7517d
Vasyl Horaichuk <vg.createam@gmail.com>
2023-03-20 22:12:17 +0200

    update

--- src/components/AddWordsForm.jsx
+++ src/components/AddWordsForm.jsx
@@ -1,16 +1,32 @@
 import { Button, TextField } from '@mui/material';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import GTranslateIcon from '@mui/icons-material/GTranslate';
+import { blue } from '@mui/material/colors';
 
 export const AddWordsForm = () => {
   return (
     <form
       style={{
+        display: 'flex',
+        alignItems: 'center',
+        gap: '20px',
         padding: '24px',
         backgroundColor: 'white',
       }}
     >
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField
+        id="outlined-basic"
+        label="Outlined"
+        variant="outlined"
+        size="small"
+      />
+      <GTranslateIcon sx={{ color: blue[500] }} />
+      <TextField
+        id="outlined-basic"
+        label="Outlined"
+        variant="outlined"
+        size="small"
+      />
 
       <Button startIcon={<AddCircleOutlineIcon />} variant="contained">
         Add
