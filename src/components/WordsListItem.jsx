eb4ddb3f7734cd88c8e38073d295a9604dc60b36
Vasyl Horaichuk <vg.createam@gmail.com>
2023-03-20 16:18:11 +0200

    update

--- src/components/WordsListItem.jsx
+++ src/components/WordsListItem.jsx
@@ -1,9 +1,13 @@
-import { FormControlLabel, Checkbox } from '@mui/material';
+import { FormControlLabel, Checkbox, Button } from '@mui/material';
 
 export const WordsListItem = () => {
   return (
     <li>
       <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+      <span>UA</span>
+      <span>ENG</span>
+      <Button>Edit</Button>
+      <Button>Delete</Button>
     </li>
   );
 };
