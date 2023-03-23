ac87b062e2d167ab1cf5bf2b60ff418ceeac8742
Vasyl Horaichuk <vg.createam@gmail.com>
2023-03-23 21:39:51 +0200

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { wordsReducer } from './wordSlice';
 
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['words'],
+};
+
+const reducer = combineReducers({ words: wordsReducer });
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 export const store = configureStore({
-  reducer: {
-    words: wordsReducer,
-  },
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
