7c4484c75c369f642d05d042adeb2aa0a52720df
Vasyl Horaichuk <vg.createam@gmail.com>
2023-03-23 13:14:14 +0200

    update

--- src/components/WordsListItem.jsx
+++ src/components/WordsListItem.jsx
@@ -3,6 +3,7 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh';
 import { useDispatch } from 'react-redux';
 import { deleteWord, editWord } from 'redux/wordSlice';
+import { useState } from 'react';
 
 export const WordsListItem = ({
   word: { ukrWord, engWord, checked, id },
@@ -10,6 +11,20 @@ export const WordsListItem = ({
 }) => {
   const dispatch = useDispatch();
   const [isEdit, setIsEdit] = useState(false);
+  const [ukrVariant, setUkrVariant] = useState(ukrWord);
+  const [engVariant, setEngVariant] = useState(engWord);
+
+  const handleEdit = e => {
+    if (isEdit) {
+      dispatch(
+        editWord({ id, ukrWord: ukrVariant, engWord: engVariant, checked })
+      );
+      setIsEdit(false);
+      return;
+    }
+    setIsEdit(true);
+  };
+
   return (
     <li
       style={{
@@ -23,8 +38,28 @@ export const WordsListItem = ({
         label="Label"
       />
       <span>{number}</span>
-      {isEdit ? <input type="text" /> : <span>{ukrWord}</span>}
-      {isEdit ? <input type="text" /> : <span>{engWord}</span>}
+      {isEdit ? (
+        <input
+          type="text"
+          value={ukrWord}
+          onChange={e => {
+            setUkrVariant(e.targetvalue);
+          }}
+        />
+      ) : (
+        <span>{ukrWord}</span>
+      )}
+      {isEdit ? (
+        <input
+          type="text"
+          value={engWord}
+          onChange={e => {
+            setEngVariant(e.targetvalue);
+          }}
+        />
+      ) : (
+        <span>{engWord}</span>
+      )}
       <div>
         <Button
           onClick={() => dispatch(editWord(id))}
